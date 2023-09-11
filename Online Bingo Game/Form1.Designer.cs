cb5ad0f860d1e661cca1a04b5cc4c4aa03fb5ede
Zbreakmath <raz.frumkin@gmail.com>
2023-09-11 13:37:24 +0300

    NO-ISSUE: Uploaded all the necessary files

---
+++ Online Bingo Game/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Online_Bingo_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.nextWord = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 74);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(359, 220);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(48, 309);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Game Port:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(196, 316);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(155, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(108, 399);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 27);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label2.Location = new System.Drawing.Point(48, 349);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Name:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(196, 356);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(155, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label3.Location = new System.Drawing.Point(48, 297);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 29);
+            this.label3.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label4.Location = new System.Drawing.Point(34, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(168, 29);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Current Word: ";
+            // 
+            // nextWord
+            // 
+            this.nextWord.AutoSize = true;
+            this.nextWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.nextWord.Location = new System.Drawing.Point(208, 27);
+            this.nextWord.Name = "nextWord";
+            this.nextWord.Size = new System.Drawing.Size(0, 29);
+            this.nextWord.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCyan;
+            this.ClientSize = new System.Drawing.Size(383, 793);
+            this.Controls.Add(this.nextWord);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Online Bingo Game";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label nextWord;
+    }
+}
+
---
+++ Online Bingo Game/Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Online_Bingo_Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            inilializeBoard();
+        }
+        int port;
+        int winOpt; //determines the win method
+        Thread connection;
+        TcpClient client = new TcpClient(); //The client object
+        Button[,] board = new Button[3,3];
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            //MessageBox.Show(e.X.ToString() + "," + e.Y.ToString());
+        }
+        private void inilializeBoard() 
+        {
+            int x = 2, y = 359;
+            int side = 282 / 3;
+            for (int row = 0; row < 3; row++)
+                for (int column = 0; column < 3; column++)
+                {
+                    Button cell = new Button();
+                    cell.Text = "";
+                    cell.Font = new Font("Arial", 12, GraphicsUnit.Pixel);
+                    cell.BackColor = Color.LightSkyBlue;
+                    cell.Location = new Point(x + side * column, y + side * row);
+                    cell.Size = new Size(side, side);
+                    Controls.Add(cell);
+                    //cell.Click += doTurn; //Attaching the event
+                    board[row, column] = cell;
+                }
+        }
+        private void connect()
+        {
+            port = int.Parse(textBox1.Text);
+            try
+            {
+                client.Connect("127.0.0.1", port); //Connects to the remote server
+
+                connection = new Thread(receiveData);
+                connection.Start();
+                button1.Enabled = false;
+                sendData(textBox2.Text);
+                richTextBox1.AppendText("Connected Successfully!");
+                richTextBox1.AppendText(Environment.NewLine);
+            }
+            catch
+            {
+                richTextBox1.AppendText("Unable to connect. Try again.");
+                richTextBox1.AppendText(Environment.NewLine);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            connect();
+        }
+        private void receiveData() 
+        {
+            
+                string data = "";
+                while (true)
+                {
+                    try
+                    {
+                        byte[] stream = new byte[100025];
+                        NetworkStream clientS = client.GetStream();
+                        clientS.Read(stream, 0, client.ReceiveBufferSize);
+                        data = Encoding.ASCII.GetString(stream);
+                        data = data.Substring(0, data.IndexOf("$"));
+                        handleData(data);
+                    }
+                    catch 
+                    {
+                        if (this.InvokeRequired)
+                        {
+                            Invoke(new Action(() =>
+                            {
+                                button1.Enabled = true;
+                            }));
+                        }
+                        else
+                        {
+                            button1.Enabled = true;
+                            connection.Abort();
+                        }
+                    }
+                }
+            
+        }
+        private void handleData(string newData)
+        {
+            string type = newData.Substring(0, newData.IndexOf(":")); //Gets the type of the message (which is declared at the start until the ':'
+            switch (type)
+            {
+                case "WINOPT":
+                    winOpt = int.Parse(newData.Remove(0,newData.IndexOf(":") + 1));
+                    break;
+                case "WORDLIST":
+                    if (this.InvokeRequired)
+                    {
+                        Invoke(new Action(() =>
+                        {
+                            synchronizeBoards(generateBoard(newData.Remove(0, newData.IndexOf(":") + 1)));
+                        }));
+                    }
+                    else
+                    {
+                        synchronizeBoards(generateBoard(newData.Remove(0, newData.IndexOf(":") + 1)));
+                    }
+                    break;
+                case "WORD":
+                    if (this.InvokeRequired)
+                    {
+                        Invoke(new Action(() =>
+                        {
+                            wordHandle(newData.Remove(0, newData.IndexOf(":") + 1));
+                        }));
+                    }
+                    else
+                    {
+                        wordHandle(newData.Remove(0, newData.IndexOf(":") + 1));
+                    }
+                    break;
+                case "MESSAGE":
+                    printData(newData.Remove(0,newData.IndexOf(":") + 1));
+                    break;
+                default: 
+                    break;
+            }
+        }
+        private string[,] generateBoard(string list) 
+        {
+            string[,] temp = new string[3, 3];
+            string part = list;
+            for(int row = 0; row < temp.GetLength(0);row++)
+                for (int item = 0; item < temp.GetLength(1); item++)
+                {
+                    part = list.Substring(0, list.IndexOf("|")); //Gets the next word in the list
+                    list = list.Remove(0, list.IndexOf("|") + 1); //Removes the current word from the list
+                    temp[row, item] = part;
+                }
+            return temp;
+        }
+        private void synchronizeBoards(string[,] sBoard)
+        { 
+            for(int row = 0; row < board.GetLength(0);row++)
+                for (int item = 0; item < board.GetLength(1); item++)
+                    board[row, item].Text = sBoard[row, item];
+        }
+        private void sendData(string data) 
+        { 
+            try
+            {
+                NetworkStream clientS = client.GetStream();
+                byte[] stream;
+                stream = Encoding.ASCII.GetBytes(data + "$");
+                clientS.Write(stream, 0, stream.Length);
+                clientS.Flush();
+            }
+            catch
+            {
+                connection.Abort();
+                client.Close();
+                richTextBox1.AppendText("You were disconnected.");
+                richTextBox1.AppendText(Environment.NewLine);
+            }
+        }
+        private void printData(string data)
+        {
+            if (this.InvokeRequired)
+            {
+                Invoke(new Action(() =>
+                {
+                    richTextBox1.AppendText(data);
+                    richTextBox1.AppendText(Environment.NewLine);
+                }));
+            }
+            else
+            {
+                richTextBox1.AppendText(data);
+                richTextBox1.AppendText(Environment.NewLine);
+            }
+        }
+        private void wordHandle(string word)
+        {
+            nextWord.Text = word; //Updates the word
+            for (int i = 0; i < board.GetLength(0); i++)
+                for (int j = 0; j < board.GetLength(1); j++)
+                    if (board[i, j].Text.Equals(word))
+                    {
+                        board[i, j].Enabled = false;
+                        board[i, j].BackColor = Color.Wheat;
+                    }
+            //Check for winner
+            if (winOpt == 1)
+            {
+                if (!board[0, 0].Enabled && !board[0, 1].Enabled && !board[0, 2].Enabled)
+                    sendData("BINGO");
+                else if (!board[1, 0].Enabled && !board[1, 1].Enabled && !board[1, 2].Enabled)
+                    sendData("BINGO");
+                else if (!board[2, 0].Enabled && !board[2, 1].Enabled && !board[2, 2].Enabled)
+                    sendData("BINGO");
+                else if (!board[0, 0].Enabled && !board[1, 0].Enabled && !board[2, 0].Enabled)
+                    sendData("BINGO");
+                else if (!board[0, 1].Enabled && !board[1, 1].Enabled && !board[2, 1].Enabled)
+                    sendData("BINGO");
+                else if (!board[0, 2].Enabled && !board[1, 2].Enabled && !board[2, 2].Enabled)
+                    sendData("BINGO");
+                else if (!board[0, 0].Enabled && !board[1, 1].Enabled && !board[2, 2].Enabled)
+                    sendData("BINGO");
+                else if (!board[2, 0].Enabled && !board[1, 1].Enabled && !board[0, 2].Enabled)
+                    sendData("BINGO");
+            }
+            else 
+            { 
+                bool found = false;
+                for (int row = 0; row < board.GetLength(0); row++)
+                    for (int item = 0; item < board.GetLength(1); item++)
+                        if (board[row, item].Enabled)
+                            found = true;
+                if (!found)
+                    sendData("BINGO");
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            client.Close();
+        }
+    }
+}
